9cf07d13b6a7c55af8cf435c56ed5e2cf72df8d5
David Berkheimer <berkheimer112@atlanticbb.net>
2019-09-05 17:25:20 -0400

    cleaned up elementsJS

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -1,31 +1,30 @@
 class Elements {
   constructor() {
-    //select the lists
+    // select the lists
     this.fileCabList = document.querySelector("#fileCabList");
     this.mainFolderList = document.querySelector("#mainFolderList");
     this.subFolderList = document.querySelector("#subFolderList");
     this.noteList = document.querySelector("#noteList");
-    // this.noteHeader = document.querySelector("#headingNote");
-    //Select headings
+    // select headings
     this.mfHeading = document.querySelector("#headingMainFolder");
     this.sfHeading = document.querySelector("#headingSubFolder");
     this.nHeading = document.querySelector("#headingNote");
-    //Select forms
+    // select forms
     this.renameFileCabForm = document.querySelector("#renameFileCabForm");
     this.mainFolderForm = document.querySelector("#mainFolderForm");
     this.subFolderForm = document.querySelector("#subFolderForm");
     this.noteForm = document.querySelector("#noteForm");
     this.settingsForm = document.querySelector("#settingsForm");
-    //Select add show forms +
+    // select add show forms + / icon
     this.addShowFormMain = document.querySelector("#mfadd");
     this.addShowFormSub = document.querySelector("#sfadd");
     this.addShowFormNote = document.querySelector("#nadd");
-    //Select textName and textArea
+    // select textName and textArea
     this.textRenameFileCab = document.querySelector("#newFileCabName");
     this.textNameMain = document.querySelector("#mainFolderName");
     this.textNameSub = document.querySelector("#subFolderName");
     this.textArea = document.querySelector("#myTextArea");
-    // autoload
+    // select the autoload list
     this.autoLoadList = document.querySelector("#autoLoadList");
   } //constructor
 }
