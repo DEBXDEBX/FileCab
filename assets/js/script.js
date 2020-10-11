eb9750ece1d5470a45ac91a7c4ebfa4e53ab119e
David Berkheimer <berkheimer112@atlanticbb.net>
2020-10-11 16:08:32 -0400

    tookout comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,7 +49,6 @@ const display = new Display(el, $);
 // This is the Main array that holds all the file cab objects
 const arrayOfFileCabs = [];
 
-//When you click on the icon's the tool tip gets messed up and you can't click one it.
 // This enables JQuery ToolTips
 $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
