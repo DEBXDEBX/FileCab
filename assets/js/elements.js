4357bd6695b6d4ed5d89880430f67e96b34a4e79
David Berkheimer <berkheimer112@atlanticbb.net>
2020-10-28 16:10:47 -0400

    changed IDS

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -16,16 +16,18 @@ class Elements {
     this.noteForm = document.querySelector("#noteForm");
     this.settingsForm = document.querySelector("#settingsForm");
     // select btn
-    this.renameFileCabAdd = document.querySelector("#renameFileCabAdd");
-    this.mainFolderAddBtn = document.querySelector("#mainFolderAdd");
-    this.mainFolderRenameBtn = document.querySelector("#mainFolderRename");
-    this.subFolderAddBtn = document.querySelector("#subFolderAdd");
-    this.subFolderRenameBtn = document.querySelector("#subFolderRename");
+    this.renameFileCabAdd = document.querySelector("#renameFileCabSubmitBtn");
+    this.mainFolderAddBtn = document.querySelector("#addMainFolderSubmitBtn");
+    this.mainFolderRenameBtn = document.querySelector("#renameMainFolderBtn");
+    this.subFolderAddBtn = document.querySelector("#addSubFolderSubmitBtn");
+    this.subFolderRenameBtn = document.querySelector(
+      "#renameSubfolderSubmitBtn"
+    );
     // select textName and textArea
-    this.textRenameFileCab = document.querySelector("#newFileCabName");
-    this.textNameMain = document.querySelector("#mainFolderName");
-    this.textNameSub = document.querySelector("#subFolderName");
-    this.textArea = document.querySelector("#myTextArea");
+    this.textRenameFileCab = document.querySelector("#fileCabNameInput");
+    this.textNameMain = document.querySelector("#mainFolderNameInput");
+    this.textNameSub = document.querySelector("#subFolderNameInput");
+    this.textArea = document.querySelector("#noteTextareaInput");
     // select the autoload list
     this.autoLoadList = document.querySelector("#autoLoadList");
     // select message display
@@ -35,31 +37,33 @@ class Elements {
     // this is for the fontSize
     this.root = document.querySelector(":root");
     this.myBody = document.querySelector("body");
-    this.autoLoad = document.querySelector("#autoLoad");
-    this.dark = document.querySelector("#Dark");
-    this.light = document.querySelector("#Light");
-    this.xSmall = document.querySelector("#x-small");
-    this.small = document.querySelector("#small");
-    this.normal = document.querySelector("#normal");
-    this.large = document.querySelector("#large");
-    this.xLarge = document.querySelector("#x-large");
-    this.blank = document.querySelector("#blank");
-    this.renameFileCabCancel = document.querySelector("#renameFileCabCancel");
-    this.mfAdd = document.querySelector("#mfadd");
-    this.mainFolderCancel = document.querySelector("#mainFolderCancel");
-    this.sfAdd = document.querySelector("#sfadd");
-    this.subFolderCancel = document.querySelector("#subFolderCancel");
+    this.autoLoad = document.querySelector("#autoLoadCheckBox");
+    this.dark = document.querySelector("#darkRadio");
+    this.light = document.querySelector("#lightRadio");
+    this.xSmall = document.querySelector("#x-smallRadio");
+    this.small = document.querySelector("#smallRadio");
+    this.normal = document.querySelector("#normalRadio");
+    this.large = document.querySelector("#largeRadio");
+    this.xLarge = document.querySelector("#x-largeRadio");
+    this.blank = document.querySelector("#blankCssLink");
+    this.renameFileCabCancel = document.querySelector(
+      "#renameFileCabCancelBtn"
+    );
+    this.mfAdd = document.querySelector("#mainFolderAddIcon");
+    this.mainFolderCancel = document.querySelector("#mainFolderCancelBtn");
+    this.sfAdd = document.querySelector("#subFolderAddIcon");
+    this.subFolderCancel = document.querySelector("#subFolderCancelBtn");
     this.noteModalTextarea = document.querySelector("#noteModalTextarea");
-    this.nAdd = document.querySelector("#nadd");
-    this.noteAdd = document.querySelector("#noteAdd");
-    this.noteCancel = document.querySelector("#noteCancel");
-    this.noteClearTextArea = document.querySelector("#noteClearTextArea");
-    this.noteDate = document.querySelector("#noteDate");
-    this.saveEdit = document.querySelector("#saveEdit");
-    this.editClose = document.querySelector("#editClose");
-    this.settingsSave = document.querySelector("#settingsSave");
-    this.settingsCancel = document.querySelector("#settingsCancel");
-    this.factoryReset = document.querySelector("#factoryReset");
-    this.settingsAddPath = document.querySelector("#settingsAddPath");
+    this.nAdd = document.querySelector("#noteAddIcon");
+    this.noteAdd = document.querySelector("#addNoteSubmitBtn");
+    this.noteCancel = document.querySelector("#noteCancelBtn");
+    this.noteClearTextArea = document.querySelector("#noteClearTextAreaBtn");
+    this.noteDate = document.querySelector("#noteAddDateBtn");
+    this.saveEdit = document.querySelector("#saveEditedNoteBtn");
+    this.editClose = document.querySelector("#editNoteCloseBtn");
+    this.settingsSave = document.querySelector("#saveSettingsSubmitBtn");
+    this.settingsCancel = document.querySelector("#settingsCancelBtn");
+    this.factoryReset = document.querySelector("#factoryResetBtn");
+    this.settingsAddPath = document.querySelector("#settingsAddPathBtn");
   } // End constructor
 } // End Elements class
